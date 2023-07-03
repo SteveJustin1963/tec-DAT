9620f9f221e843c021130e131c5b035bc6316df0
Stephen Justin <stevejustin1963@gmail.com>
2023-07-03 22:39:16 +1000

    Update ask.c

--- ask.c
+++ ask.c
@@ -1,12 +1,12 @@
-
 #include <stdio.h>
 #include <math.h>
 
 int main()
 {
-    int i, j, k;
+    int i, binaryInput[] = {1, 0, 1, 1, 0, 0, 1, 0, 1, 1}; //binary input array
+    int n = sizeof(binaryInput)/sizeof(binaryInput[0]); //length of binary input array
     double A, f, t1, dt;
-    
+
     printf("Enter amplitude: ");
     scanf("%lf", &A);
     printf("Enter frequency: ");
@@ -16,11 +16,18 @@ int main()
     printf("Enter time interval: ");
     scanf("%lf", &dt);
     
-    for (i=0; i<10; i++) {
-        for (j=0; j<1000; j++) {
-            k = A*sin(2*M_PI*f*(t1+j*dt));
-            printf("%d\t", k);
+    for (i=0; i<n; i++) {
+        if (binaryInput[i] == 1) { // binary 1
+            for (double j = t1; j < t1 + dt; j += 0.001) {
+                double k = A * cos(2*M_PI*f*j);
+                printf("%lf\t", k);
+            }
+        } else { // binary 0
+            for (double j = t1; j < t1 + dt; j += 0.001) {
+                printf("0.000000\t"); // 0 amplitude
+            }
         }
+        t1 += dt;
         printf("\n");
     }
     return 0;
